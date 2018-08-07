100ca614fc4b91ce62c6cd43f058e15950384a79
dodying <549779963@qq.com>
2018-08-07 20:55:44 +0800

    1. 新增: 右键菜单快速添加到exclude 2. 新增: 为exclude内的网页提供特定图标 3. 修改: 图标透明化

--- src/js/background.js
+++ src/js/background.js
@@ -43,6 +43,21 @@ const removeCookie = async function (details) {
   })
 }
 
+const notification = function (text) {
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: './../images/logo-48.png',
+    title: 'One Click Clean',
+    message: text,
+    eventTime: 3 * 1000,
+    isClickable: true
+  }, function () {
+    chrome.notifications.onClicked.addListener(function (notificationId) {
+      chrome.notifications.clear(notificationId)
+    })
+  })
+}
+
 chrome.browserAction.onClicked.addListener(async function () {
   let exclude = JSON.parse(window.localStorage.exclude)
   if (!window.confirm(warning.replace(/{exclude}/, exclude.join('\n')))) return window.alert('One Click Clean Canceled')
@@ -58,16 +73,71 @@ chrome.browserAction.onClicked.addListener(async function () {
   }
   await removeOther()
 
-  chrome.notifications.create({
-    type: 'basic',
-    iconUrl: './../images/logo-48.png',
-    title: 'One Click Clean',
-    message: 'Clean Succeed',
-    eventTime: 3 * 1000,
-    isClickable: true
-  }, function () {
-    chrome.notifications.onClicked.addListener(function (notificationId) {
-      chrome.notifications.clear(notificationId)
-    })
+  notification('Clean Succeed')
+})
+
+const addToExclude = function (text) {
+  let exclude = JSON.parse(window.localStorage.exclude)
+  exclude.push(text)
+  exclude = exclude.filter(i => i)
+  window.localStorage.exclude = JSON.stringify(exclude)
+  notification(`add ${text} to Exclude`)
+}
+
+chrome.contextMenus.create({
+  title: 'Add "%s" to Exclude',
+  id: 'one-click-clean-selection',
+  contexts: ['selection']
+})
+
+chrome.contextMenus.create({
+  title: 'Add this host of src to Exclude',
+  id: 'one-click-clean-src',
+  contexts: ['image', 'video', 'audio']
+})
+
+chrome.contextMenus.create({
+  title: 'Add this host of link to Exclude',
+  id: 'one-click-clean-link',
+  contexts: ['link']
+})
+
+chrome.contextMenus.create({
+  title: 'Add this host of frame to Exclude',
+  id: 'one-click-clean-frame',
+  contexts: ['frame']
+})
+
+chrome.contextMenus.create({
+  title: 'Add this host of page to Exclude',
+  id: 'one-click-clean-page',
+  contexts: ['page', 'browser_action']
+})
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === 'one-click-clean-selection') {
+    addToExclude(info.selectionText)
+  } else if (info.menuItemId === 'one-click-clean-src') {
+    addToExclude(new URL(info.srcUrl).host)
+  } else if (info.menuItemId === 'one-click-clean-link') {
+    addToExclude(new URL(info.linkUrl).host)
+  } else if (info.menuItemId === 'one-click-clean-frame') {
+    addToExclude(new URL(info.frameUrl).host)
+  } else if (info.menuItemId === 'one-click-clean-page') {
+    addToExclude(new URL(tab.url).host)
+  }
+})
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.query({active: true}, info => {
+    let exclude = JSON.parse(window.localStorage.exclude)
+    for (let i of info) {
+      if (exclude.some(j => i.url.match(j))) {
+        chrome.browserAction.setIcon({
+          path: 'images/logo-disabled-48.png',
+          tabId: i.id
+        })
+      }
+    }
   })
 })
